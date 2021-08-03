b44f13a6b0ca3211d6065780b985d36662e1ad0f
AAAAA-Li <798489210@qq.com>
2021-08-03 16:45:36 +0800

    提交代码

---
+++ 课程1-6（选择语句）/课程1-6（选择语句）/课程1-6.c
@@ -0,0 +1,74 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//选择语句
+//int main()
+//{
+//	int num1 = 0;
+//	printf("你要好好学习吗?(1/0)>:");
+//	scanf("%d", &num1);
+//	if (num1 == 1)
+//		printf("走上人生巅峰\n");
+//	else
+//		printf("拜拜\n");
+//	return 0;
+//}
+
+
+//循环语句
+//int main()
+//{
+//	int line = 0;
+//
+//	printf("开始学习");
+//	while (line < 20000)
+//	{
+//		printf("敲一行代码 敲到: %d行", line);
+//		line++;
+//	}
+//	if (line >= 20000);
+//	printf("好offer");
+//	return 0;
+
+
+
+//int Add(int x, int y) //Add函数名称 括号内称函数的参数 下面的大括号内称函数题
+//{
+//	int z = x + y;
+//	return z;
+//}
+//
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int sum = 0;
+//	printf("请输入第一个数字：");
+//	scanf("%d", &a);
+//	printf("请输入第二个数字：");
+//	scanf("%d", &b);
+//	sum = Add(a, b);
+//	printf("%d\n", sum);
+//	return 0;
+//}
+
+//函数分库函数和自定义函数 比如上面的scanf是库函数 Add是自定义函数
+
+//int main()
+//{
+//	//int a = 1;
+//	//int b = 0;
+//	//int c = 4;
+//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		printf("%d", arr[i]);
+//		i++;
+//	}
+//	return 0;
+//}
+
+
+
+
